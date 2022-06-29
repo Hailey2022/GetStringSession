f1a2abee93ec0de8839365aeb736c4e45c423a07
Hailey2022 <96413516+Hailey2022@users.noreply.github.com>
2022-06-29 15:31:32 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+
+print("""Please go to my.telegram.org""")
+APP_ID = int(input("Enter APP ID here: "))
+API_HASH = input("Enter API HASH here: ")
+
+with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
+    print(client.session.save())
